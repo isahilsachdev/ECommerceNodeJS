d7b89f308c16664532b41bf1843095d1bc2fc25b
Sahil Sachdev <sahil.sachdev@inai.io>
2023-02-04 23:58:54 +0530

    added default endpoint, added cors

--- index.js
+++ index.js
@@ -1,11 +1,22 @@
 const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
+const cors = require('cors');
 
-mongoose.connect(
-  "mongodb+srv://isahilsachdev2002:sahilsachdev1029@cluster0.rnkg0g6.mongodb.net/?retryWrites=true&w=majority"
-).then(() => console.log("database connection successful")).catch((err) => console.log("database connection failed", err))
+app.use(cors());
 
-app.listen("5001", () => {
-  console.log("listening on port 5001")
-})
+app.get('/', (req, res) => {
+  res.send('App is running..');
+});
+
+CONNECTION_URL = "mongodb+srv://isahilsachdev2002:sahilsachdev1029@cluster0.rnkg0g6.mongodb.net/?retryWrites=true&w=majority"
+const PORT = process.env.PORT || 5001;
+
+mongoose
+  .connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() =>
+    app.listen(PORT, () =>
+      console.log(`Server Running on Port: http://localhost:${PORT}`)
+    )
+  )
+  .catch((error) => console.log(`${error} did not connect`));
